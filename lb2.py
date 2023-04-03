f0425dc89c0b47b65e996edf808392831a3c6e4a
AuzKat <125350670+AuzKat@users.noreply.github.com>
2023-04-03 23:09:30 +0300

    Add files via upload

--- lb2.py
+++ lb2.py
@@ -1,54 +1,86 @@
-def proc1():
-    n = int(input("ввод кол слов:"))  # вводим количество слов
-    result = " "  # переменная с результатом
-    for i in range(n):  # цикл в количестве n от первой строки
-        w = input(f"ввод слов {i + 1}:")  # переменная которая принимает, значение
-        result += w + " "  # прибавляем ко второй строке значение строки w
-    print("результат:", result.strip())  # выводим результат
+def lb11():
+    print('Введите пароль:')
 
-def proc2():
-    n = int(input("ввод кол слов:"))
-    result = []
-    for i in range(n):
-        result.append(str(input()))
+    a=input()
+    b=input()
 
-    result = []
-    while(new_word := str(input())) != "stop":
-        result.append(new_word)
-        print(" ".join(result))
-
-
-def proc3():
-    result = []
-    str(input())
-    if "ф" or  "Ф" in str:
-            print("Ого! Это редкое слово!")
+    if a==b:
+        print('Пароль совпадает')
     else:
-        print("Эх,это не очень редкое слово")
+        print('Пароль не совпадает')
 
-def proc4():
-    import random
-    a = random.randint(0, 100) # вводим первую переменную в диапазоне 0-99
-    b = random.randint(0, 100) # вводим вторую переменную в диапазоне 0-99
-    s = a + b # s-сумма двух переменных
-    print(a1, '+', b1, '=')
-    o = int(input()) # выводим целое число в ответ
-    if o == s: # сравниваем ответ с суммой
-        print('Правильно!')
+
+def lb12():
+    password = input('Введите пароль:')
+    if len(password) < 6:
+        print('Слишком короткий пароль')
+    elif password[0:7] == "abricos":
+        print('Ненадёжный пароль')
     else:
-        print('Ответ неверный')
+        print('Надёжный пароль')
 
-def proc41():
-    import random
-    error_count = 0
-    count = 0
-    while error_count < 3:
-        a = random.randint(0, 100)
-        b = random.randint(0, 100)
-        if int(input(f"{a} + {b} = ")) == a + b:
-            count += 1
-            print('Правильно!')
+def lb13():
+    password = input('Введите пароль')
+    is_numeric = False
+    is_upper = False
+    is_lower = False
+    is_spec = False
+
+    for char in password:
+        if char.isnumeric():
+            is_numeric = True
+        elif char.islower():
+            is_lower = True
+        elif char.isupper():
+            is_upper = True
+        elif char in "@#%&":
+            is_spec = True
+    if len(password) > 4 and is_numeric and is_upper and is_lower and is_spec:
+        print('Пароль: подходит')
+    else:
+        print('Пароль прост')
+
+def lb21():
+    n = int(input('Ввод номера места:'))
+    print()
+    if n > 36:
+        print('Ваше место - боковое')
+    elif n % 2:
+        print('Ваше место в купе внизу')
+    else:
+        print('Ваше место в купе наверху')
+
+def lb22():
+    n = int(input('Ввод номера места:'))
+    print()
+    if n > 36:
+        print('Ваше место - боковое')
+    elif n % 2:
+        print('Ваше место в купе внизу')
+    else:
+        print('Ваше место в купе наверху')
+
+def lb23():
+    year = int(input())
+    if (year % 4 == 0) and (year % 100 != 0) or (year % 400 == 0):
+        print('Високосный')
+    else:
+        print('Не високосный')
+
+def lb24():
+    colors = ('красный', 'синий', 'желтый')
+    c1 = input()
+    c2 = input()
+    if c1 in colors and c2 in colors:
+        if c1 != c2:
+            if (c1 in ("красный", "синий")) and (c2 in ("красный", "синий")):
+                print("фиолетовый")
+        elif (c1 in ("желтый", "красный")) and (c2 in ("желтый", "красный")):
+            print("оранжевый")
+        elif (c1 in ("синий", "желтый")) and (c2 in ("синий", "желтый")):
+            print("зеленый")
         else:
-            error_count += 1
-            print('Неправильно!')
-    print(f"Игра окончена. Правильных ответов {count}")
+            print(c1)
+    else:
+        print("ошибка цвета")
+        
